b6917d825fb7d0e18ac1c581f9a9345953a0021b
Hans Delano <hanscau@hotmail.com>
2024-01-19 14:50:06 +0800

    add user model

---
+++ db/migrate/20240119064926_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :firstName
+      t.string :lastName
+      t.string :username
+      t.text :bio
+      t.integer :gender
+      t.string :phoneNo
+      t.string :profileImageURL
+
+      t.timestamps
+    end
+  end
+end
---
+++ setup.sh
@@ -0,0 +1,1 @@
+bin/rails g scaffold User firstName:string lastName:string username:string bio:text gender:integer phoneNo:string profileImageURL:string --force
